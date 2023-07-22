e81a8268ab45a0b05796425f971615789d4a558d
nichgalzin <nichgalzin@gmail.com>
2023-07-22 17:26:38 +0100

    Refactoring

--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -4,87 +4,59 @@ const values = require('./utils/values');
 
 describe('assertEquals', () => {
   
-  // Strings:
-  describe('when expected and actual are the same string', () => {
+  // Strings, numbers, booleans, null or undefined
+  describe('when expected and actual are the same primitive value', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals('abc', 'abc')).not.toThrow()
-      
+      expect(() => assertEquals(1, 1)).not.toThrow();
+      expect(() => assertEquals(true, true)).not.toThrow();
+      expect(() => assertEquals(null, null)).not.toThrow();
+      expect(() => assertEquals(undefined, undefined)).not.toThrow();
     })
   })
 
-  describe('when expected and actual are different strings', () => {
+  describe('when expected and actual are different primitive values', () => {
     it('returns an error when expected and actual are different', () => {
       expect(() => assertEquals('abcef', 'abc')).toThrow();
-    })
-  })
-
-  // Numbers
-  describe('when expected and actual are the same number', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals(1, 1)).not.toThrow();
-    })
-  })
-
-  describe('when expected and actual are the different numbers', () => {
-    it('returns an error when expected and actual are different', () => {
       expect(() => assertEquals(1, 2)).toThrow();
+      expect(() => assertEquals(true, false)).toThrow();     
+      expect(() => assertEquals(null, undefined)).toThrow(); 
     })
   })
 
   // Primitive data types
-
   describe('when expected and actual are the same primitive type', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals(typeof 1, typeof 1)).not.toThrow();
+      expect(() => assertEquals(typeof true, typeof false)).not.toThrow();     
     })
   })
 
   describe('when expected and actual are the different primitive type', () => {
     it('returns an error when expected and actual are different', () => {
       expect(() => assertEquals(typeof 1, typeof '1')).toThrow();
+      expect(() => assertEquals(typeof null, typeof undefined)).toThrow();     
     })
   })
 
-  // Array lengths 
-  describe('when expected and actual arrays are the same length', () => {
+  // Array lengths and values
+  describe('when expected and actual arrays values or length are the same', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals(values.arrA.length, values.arrC.length)).not.toThrow();
-    })
-  })
-
-  describe('when expected and actual arrays are different lengths', () => {
-    it('returns an error when expected and actual are different', () => {
-      expect(() => assertEquals(values.arrA.length, values.arrB.length)).toThrow();
-    })
-  })
-
-  // Array values  
-  describe('when a value at index of expected and actual arrays are the same', () => {
-    it('returns without throwing an error', () => {
+      expect(() => assertEquals(values.arrA[1], values.arrC[1])).not.toThrow();
       expect(() => assertEquals(values.arrA, values.arrC)).not.toThrow();
     })
   })
 
-  describe('when a value at index of expected and actual arrays are different', () => {
+  describe('when expected and actual arrays values or lengths are different', () => {
     it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(values.arrA.length, values.arrB.length)).toThrow();
+      expect(() => assertEquals(values.arrA[3], values.arrD[3])).toThrow();
       expect(() => assertEquals(values.arrA, values.arrD)).toThrow();
     })
   })
 
-  describe('when every value of expected and actual arrays are the same', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals(values.arrA[1], values.arrC[1])).not.toThrow();
-    })
-  })
-
-  describe('when not every value of expected and actual arrays are the same', () => {
-    it('returns an error when expected and actual are different', () => {
-      expect(() => assertEquals(values.arrA[3], values.arrD[3])).toThrow();
-    })
-  })
-
   // Output of a function 
-
   describe('when expected and actual function output are the same', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals(sum(2, 3), 5)).not.toThrow();
@@ -96,24 +68,4 @@ describe('assertEquals', () => {
       expect(() => assertEquals(sum(2, 3), 8)).toThrow();
     })
   })
-
-  // Null and undefined 
-  describe('when expected and actual are null', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals(null, null)).not.toThrow();
-    })
-  })
-
-  describe('when expected and actual are undefined', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals(undefined, undefined)).not.toThrow();
-    })
-  })
-
-  describe('when expected and actual are null and undefined', () => {
-    it('returns an error when expected and actual are different', () => {
-      expect(() => assertEquals(null, undefined)).toThrow();
-    })
-  })
-
 })
