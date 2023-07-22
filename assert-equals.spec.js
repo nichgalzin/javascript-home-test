285a57550033ca4129631aba6692c8c2773ef248
nichgalzin <nichgalzin@gmail.com>
2023-07-22 16:49:48 +0100

    Added values.js and sum.js files and content and wrote additional tests to support the working of assertEquals

--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,8 +1,10 @@
 const assertEquals = require('./assert-equals')
+const sum = require('./utils/sum');
+const values = require('./utils/values');
 
 describe('assertEquals', () => {
   
-  // Check strings:
+  // Strings:
   describe('when expected and actual are the same string', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals('abc', 'abc')).not.toThrow()
@@ -16,7 +18,7 @@ describe('assertEquals', () => {
     })
   })
 
-  // Check numbers
+  // Numbers
   describe('when expected and actual are the same number', () => {
     it('returns without throwing an error', () => {
       expect(() => assertEquals(1, 1)).not.toThrow();
@@ -29,7 +31,7 @@ describe('assertEquals', () => {
     })
   })
 
-  // Check primitive data types
+  // Primitive data types
 
   describe('when expected and actual are the same primitive type', () => {
     it('returns without throwing an error', () => {
@@ -40,6 +42,77 @@ describe('assertEquals', () => {
   describe('when expected and actual are the different primitive type', () => {
     it('returns an error when expected and actual are different', () => {
       expect(() => assertEquals(typeof 1, typeof '1')).toThrow();
+    })
+  })
+
+  // Array lengths 
+  describe('when expected and actual arrays are the same length', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(values.arrA.length, values.arrC.length)).not.toThrow();
+    })
+  })
+
+  describe('when expected and actual arrays are different lengths', () => {
+    it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(values.arrA.length, values.arrB.length)).toThrow();
+    })
+  })
+
+  // Array values  
+  describe('when a value at index of expected and actual arrays are the same', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(values.arrA, values.arrC)).not.toThrow();
+    })
+  })
+
+  describe('when a value at index of expected and actual arrays are different', () => {
+    it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(values.arrA, values.arrD)).toThrow();
+    })
+  })
+
+  describe('when every value of expected and actual arrays are the same', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(values.arrA[1], values.arrC[1])).not.toThrow();
+    })
+  })
+
+  describe('when not every value of expected and actual arrays are the same', () => {
+    it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(values.arrA[3], values.arrD[3])).toThrow();
+    })
+  })
+
+  // Output of a function 
+
+  describe('when expected and actual function output are the same', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(sum(2, 3), 5)).not.toThrow();
+    })
+  })
+
+  describe('when expected and actual function output are different', () => {
+    it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(sum(2, 3), 8)).toThrow();
+    })
+  })
+
+  // Null and undefined 
+  describe('when expected and actual are null', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(null, null)).not.toThrow();
+    })
+  })
+
+  describe('when expected and actual are undefined', () => {
+    it('returns without throwing an error', () => {
+      expect(() => assertEquals(undefined, undefined)).not.toThrow();
+    })
+  })
+
+  describe('when expected and actual are null and undefined', () => {
+    it('returns an error when expected and actual are different', () => {
+      expect(() => assertEquals(null, undefined)).toThrow();
     })
   })
 
---
+++ utils/values.js
@@ -0,0 +1,23 @@
+const arrA = [1, 2, 3, 4];
+const arrB = [1, 2, 3];
+const arrC = [1, 2, 3, 4];
+const arrD = [1, 2, 3, 5];
+
+const objA = {
+  name: 'Poppy', 
+  hungry: true,
+}
+
+const objB = {
+  name: 'Sam',
+  hungry: true,
+}
+
+module.exports = {
+  arrA: arrA,
+  arrB: arrB,
+  arrC: arrC,
+  arrD: arrD,
+  objA: objA,
+  objB: objB
+};
